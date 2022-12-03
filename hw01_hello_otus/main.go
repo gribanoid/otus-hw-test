1fff6b0c1c70835bdec72bc55d11637c47e89a4f
gricrypto <gricrypto007@gmail.com>
2022-12-03 16:19:07 +0300

    HW1 completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	otus := "Hello, OTUS!"
+	suto := stringutil.Reverse(otus)
+	fmt.Println(suto)
 }
