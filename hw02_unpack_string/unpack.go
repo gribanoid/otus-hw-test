53e4f7b890398a772ba985e42402fb2e34cdab64
gricrypto <gricrypto007@gmail.com>
2022-12-10 07:16:09 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,60 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+type myRune rune
+
+func Unpack(s string) (string, error) {
+	ans := make([]rune, 0, len(s))
+	runes := []myRune(s)
+	if isInt(0, runes) {
+		return "", ErrInvalidString
+	}
+	var curRune myRune
+	for i, val := range runes {
+		n, ok := val.ConvertToInt()
+		if !ok {
+			curRune = val
+			if isInt(i+1, runes) {
+				continue
+			}
+			ans = append(ans, rune(curRune))
+			continue
+		}
+		if isInt(i+1, runes) {
+			return "", ErrInvalidString
+		}
+		for j := 0; j < n; j++ {
+			ans = append(ans, rune(curRune))
+		}
+	}
+	return string(ans[:]), nil
+}
+
+// ConvertToInt reports whether the rune is an int digit and returns it.
+func (r myRune) ConvertToInt() (int, bool) {
+	if unicode.IsDigit(rune(r)) {
+		buf := make([]byte, 1)
+		_ = utf8.EncodeRune(buf, rune(r))
+		v, _ := strconv.Atoi(string(buf))
+		return v, true
+	}
+
+	return 0, false
+}
+
+// isInt returns whether the i-th element of the slice is int.
+// If element not found, return false.
+func isInt(i int, slice []myRune) bool {
+	if i >= len(slice) {
+		return false
+	}
+	_, ok := slice[i].ConvertToInt()
+	return ok
 }
