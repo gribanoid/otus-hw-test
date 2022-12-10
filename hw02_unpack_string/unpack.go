de228508ac1ff386f3d856cbc083204be453a7e6
gribanoid <griban2002griban@gmail.com>
2022-12-10 09:04:34 +0300

    add unit test

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,29 +9,30 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-type myRune rune
+type MyRune rune
 
 func Unpack(s string) (string, error) {
 	ans := make([]rune, 0, len(s))
-	runes := []myRune(s)
-	if isInt(0, runes) {
+	r := []MyRune(s)
+	if isInt(0, r) {
 		return "", ErrInvalidString
 	}
-	var curRune myRune
-	for i, val := range runes {
+	var curRune MyRune
+	for i, val := range r {
 		n, ok := val.ConvertToInt()
 		if !ok {
 			curRune = val
-			if isInt(i+1, runes) {
-				continue
-			}
 			ans = append(ans, rune(curRune))
 			continue
 		}
-		if isInt(i+1, runes) {
+		if isInt(i+1, r) {
 			return "", ErrInvalidString
 		}
-		for j := 0; j < n; j++ {
+		if n == 0 {
+			ans = ans[:len(ans)-1]
+			continue
+		}
+		for j := 0; j < n-1; j++ {
 			ans = append(ans, rune(curRune))
 		}
 	}
@@ -39,20 +40,19 @@ func Unpack(s string) (string, error) {
 }
 
 // ConvertToInt reports whether the rune is an int digit and returns it.
-func (r myRune) ConvertToInt() (int, bool) {
+func (r MyRune) ConvertToInt() (int, bool) {
 	if unicode.IsDigit(rune(r)) {
 		buf := make([]byte, 1)
 		_ = utf8.EncodeRune(buf, rune(r))
 		v, _ := strconv.Atoi(string(buf))
 		return v, true
 	}
-
 	return 0, false
 }
 
 // isInt returns whether the i-th element of the slice is int.
 // If element not found, return false.
-func isInt(i int, slice []myRune) bool {
+func isInt(i int, slice []MyRune) bool {
 	if i >= len(slice) {
 		return false
 	}
