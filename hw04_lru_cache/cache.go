de22a2e4da9b0a535973a0ecd35556eb38404280
gribanoid <gricrypto007@gmail.com>
2023-01-08 10:32:03 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,11 +11,47 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	mu       sync.RWMutex
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	l.mu.RLock()
+	v, ok := l.items[key]
+	l.mu.RUnlock()
+	if ok {
+		v.Value = value
+		l.queue.MoveToFront(v)
+		return true
+	}
+	listItem := l.queue.PushFront(value)
+	l.mu.Lock()
+	l.items[key] = listItem
+	l.mu.Unlock()
+	if l.queue.Len() > l.capacity {
+		l.queue.Remove(l.queue.Back())
+	}
+	return false
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	l.mu.RLock()
+	v, ok := l.items[key]
+	l.mu.RUnlock()
+	if ok {
+		l.queue.MoveToFront(v)
+		return l.queue.Front().Value, true
+	}
+	return nil, false
+}
+
+func (l *lruCache) Clear() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.items = map[Key]*ListItem{}
+	l.queue = nil
 }
 
 type cacheItem struct {
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -16,9 +20,79 @@ type ListItem struct {
 	Prev  *ListItem
 }
 
+func (l *list) Len() int {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.len++
+	front := &ListItem{Value: v}
+	if l.len == 1 {
+		l.head, l.tail = front, front
+		return front
+	}
+	l.head.Prev, front.Next = front, l.head
+	l.head = front
+	return front
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.len++
+	back := &ListItem{Value: v}
+	if l.len == 1 {
+		l.head, l.tail = back, back
+		return back
+	}
+	l.tail.Next, back.Prev = back, l.tail
+	l.tail = back
+	return back
+}
+
+func (l *list) Remove(i *ListItem) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.len--
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	// undefined behavior in goroutines
+	if i == l.head {
+		return
+	}
+	l.Remove(i)
+	l.PushFront(i.Value)
+
+}
+
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len  int
+	head *ListItem
+	tail *ListItem
+	mu   sync.RWMutex
 }
 
 func NewList() List {
